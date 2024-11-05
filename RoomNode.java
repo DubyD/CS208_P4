7760ddeb37f84dcf773d9250c3d58d1973696469
Augustus Warmington <guswarmington@gmail.com>
2024-11-05 12:45:03 -0500

    RoomNode door integer
    Explained 'door' int value. Read closely to understand how to determine how many doors and in which direction they are in.

--- RoomNode.java
+++ RoomNode.java
@@ -1,2 +1,14 @@
 public class RoomNode {
+
+
+    public RoomNode(){
+        int door = 0; /* Door has a value of 0 - 15.
+                             8 (N)
+                        (W)4   2 (E)
+                             1 (S)
+                        Each value means there is a door on that cardinal side.
+                        For example, a combined door value of 12 would mean that there are a north and west doors
+                        1 would mean only a south door. 3 = 2 + 1 (E + S)
+                        */
+    }
 }
