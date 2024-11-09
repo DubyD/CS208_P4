beef0f26c6424a6857a7fe2019e6a877382ea0cd
AliMeanLion <jasperputcasper@outlook.com>
2024-11-09 07:49:25 -0800

    I made frame1 and frame2

--- Main.java
+++ Main.java
@@ -4,20 +4,20 @@ public class Main {
     //testing testing
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Mazed and Confused");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800, 600);
-            frame.setContentPane(new TitleScene());
+            JFrame frame1 = new JFrame("Mazed and Confused");
+            frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame1.setSize(800, 600);
+            frame1.setContentPane(new TitleScene());
 
-            new SceneSwitcher(frame);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+            new SceneSwitcher(frame1);
+            frame1.setLocationRelativeTo(null);
+            frame1.setVisible(true);
         });
-        JFrame frame = new JFrame("Maze Game");
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.add(new GameFrame());
-        frame.setVisible(true);
+        JFrame frame2 = new JFrame("Maze Game");
+        frame2.setSize(800, 600);
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2.setResizable(false);
+        frame2.add(new GameFrame());
+        frame2.setVisible(true);
     }
 }
