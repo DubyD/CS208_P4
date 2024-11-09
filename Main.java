4953a685b7ef7c2133bd94994f903ebda164ec5e
AliMeanLion <jasperputcasper@outlook.com>
2024-11-09 05:45:12 -0800

    nothing changed

--- Main.java
+++ Main.java
@@ -7,9 +7,9 @@ public class Main {
             JFrame frame = new JFrame("Mazed and Confused");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(800, 600);
+            frame.setContentPane(new TitleScene());
 
             new SceneSwitcher(frame);
-
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
